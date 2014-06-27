b4d900371221064ed879a37035d1577bf5479f01
ArturoPetit <apetit.w@gmail.com>
2014-06-27 09:15:16 -0500

    BLOG/German-Petit,contra

--- editarcontra.php
+++ editarcontra.php
@@ -5,12 +5,6 @@
   session_start();
   if (isset($_SESSION['tipo'])) {
     if ($_SESSION['tipo']==1) {
-      #Link de regreso
-      echo "<table border='0' width=90% align='center'>";
-      echo "<tr>";
-      echo "<td align='right'><a href='index.php'>BLOG Super overpower</a></td>";
-      echo"</tr>";
-      echo "</table>";
       if (isset($_POST['contra'])) {
       $idcontra=$_POST['contraid'];
         if (isset($_SESSION["idusr"])) {
@@ -39,27 +33,47 @@
 <!DOCTYPE HTML>
 <META CHARSET="UTF-8">
 <html>
-  <head><!-- Bootstrap -->
+  <head>
+<title>Fix it</title></head>
+    <!-- Bootstrap -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
 <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
 <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]--><title>Fix</title></head>
+<![endif]-->
   <body>
-    <form action="editarcontra.php" method="POST" name="password">  
-    <table>     
-      <tr><td>Aregla el desperfecto</td></tr>
-        <tr><td><input type="hidden" name="contraid" value="<?php echo $contraid; ?>"></td></tr>
-        <tr><td><textarea name="contra" id="contra" cols="11" rows="2" placeholder="aqui va"><?php echo $contra; ?></textarea></td></tr>
-        <tr><td align="center"><input type="submit" value="Areglar"></td></tr>
-      </table>
-    </form>
+
+   <div class="container">
+      <!--barra de paginas-->
+      <ul class="nav nav-tabs" role="tablist">
+        <li><a href="index.php"><span class="glyphicon glyphicon-home"></span></a></li>
+        <li class="active"><a href="">Cambiar Contraseña</a></li>
+         <li><a href="nuevotema.php">Nuevo Tema</a></li>
+        <li><a href="editarusuarios.php">Editar Usuarios</a></li>
+        <li><a href="cerrar.php">Salir</a></li>
+      </ul>
+      <div class="row">
+      <form action="editarcontra.php" method="post" name="password" class="form-horizontal" role="form">
+        <div class="form-group">
+          <label for="txtcon" class="col-sm-3 control-label">Contraseña Pasada:</label>
+          <div class="col-sm-2">
+            <input type="hidden" name="contraid" value="<?php echo $contraid; ?>">
+            <input type="text" name="contra" id="contra" class="form-control" value="<?php echo $contra; ?>">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-offset-4 col-sm-5">
+            <button type="submit" value="Areglar" class="btn btn-default">Enviar</button>
+          </div>
+        </div>
+        </form>
+      </div>
+    </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
-<form action="">
